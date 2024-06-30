db17942f3383aaeb9a510f4d5c273a2ec61e3ee9
Your Name <your_email@example.com>
2024-07-01 00:33:51 +0300

    nerabotaetcid

--- src/ui/Card/Card.tsx
+++ src/ui/Card/Card.tsx
@@ -49,13 +49,10 @@ const Card: React.FC = () => {
 
     return (
         <div className="flex flex-wrap justify-center w-full">
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : (
-                vacancy.map((vacancy) => (
-                    <div key={vacancy._id} className="card w-96 glass m-4">
-                        <figure>
-                            {vacancy.image ? (
+        {vacancy.map((vacancy: Vacancy, index: number) => (
+            <div key={index} className="card w-96 glass m-4">
+                <figure>
+                {vacancy.image ? (
                                 <Image
                                     width={400}
                                     height={400}
@@ -69,33 +66,33 @@ const Card: React.FC = () => {
                                     src="/default-image.png"
                                     alt="Default Image"
                                 />
-                            )}
-                        </figure>
-                        <div className="card-body">
-                            <h2 className="card-title font-bold">{vacancy.job_title}</h2>
-                            <p className="text-md font-semibold mt-2">Location: {vacancy.location}</p>
-                            {vacancy.roof_type && (
-                                <p className="text-sm font-bold">Roof Type: {vacancy.roof_type}</p>
-                            )}
-                            <p className="text-sm font-bold">Salary: {vacancy.salary}</p>
-                            <p className="text-sm font-bold">Home Price: {vacancy.homePrice}</p>
-                            <p className="text-sm font-bold">Auto: {vacancy.auto}</p>
-                            <p className="text-sm font-bold">Documents: {vacancy.documents}</p>
-                            <div className="card-actions justify-around items-center mt-4">
-                                <a href={vacancy.viber} target='_blank'><Viber width={30} height={30} /></a>
-                                <a href={vacancy.telegram} target='_blank'><Telegram width={30} height={30} /></a>
-                                <a href={vacancy.whatsapp} target='_blank'><WhatsApp width={30} height={30} /></a>
-                                <div className="self-end">
-                                    <Link href={`/vacancy/${vacancy._id}`}>
-                                        <Button text="Details" className="btn-outline btn-error" />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
+                            )}                </figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold">{vacancy.job_title}</h2>
+                    <p className="text-md font-semibold mt-2">📍<i className="bi bi-geo-alt-fill text-red-500"></i> {vacancy.location}</p>
+                    {vacancy.roof_type && (
+    <span className="text-muted text-sm">
+      ⚙️ <i className="bi bi-dash-lg text-red-700 font-bold">{vacancy.roof_type}</i>
+    </span>
+  )}                        <p className="text-sm font-bold">💰 <i className="bi bi-cash ">Зарплата</i>&nbsp; {vacancy.salary}</p>
+                    <p className="text-sm font-bold">🏠 <i className="bi bi-cash ">Проживание</i>&nbsp; {vacancy.homePrice}</p>
+                    <p className="text-sm font-bold">🚘 <i className="bi bi-cash ">Транспорт</i>&nbsp; {vacancy.auto}</p>
+                    <p className="text-sm font-bold">📄 <i className="bi bi-cash ">Документы:</i><br /> {vacancy.documents}</p>
+
+                    <div className="card-actions justify-around items-center mt-4">
+                        <a href={vacancy.viber} target='blank'><Viber width={30} height={30} /></a>
+                        <a href={vacancy.telegram} target='blank'><Telegram width={30} height={30} /></a>
+                        <a href={vacancy.whatsapp} target='blank'><WhatsApp width={30} height={30} /></a>
+                        <div className="self-end">
+<Link href={`/vacancy/${vacancy._id}`}>
+<Button text={"Подробнее"} className='btn-outline btn-error'/>
+</Link>
+</div>
                     </div>
-                ))
-            )}
-        </div>
+                </div>
+            </div>
+        ))}
+    </div>
     );
 };
 
