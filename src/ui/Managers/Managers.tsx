7f8496a57eef9d8b00bcebd50b40ac27f3efae7e
Your Name <your_email@example.com>
2024-07-03 16:03:39 +0300

    12351

--- src/ui/Managers/Managers.tsx
+++ src/ui/Managers/Managers.tsx
@@ -1,34 +1,37 @@
-import managers from "@/lib/managers.json"
-import Image from "next/image";
-import {Telegram} from '@/svg/telegram'
-import {Viber} from '@/svg/viber'
-import {WhatsApp} from '@/svg/whatsapp'
+// src/pages/managers.tsx
+import React, { useEffect, useState } from 'react';
+import ManagerCard from '@/ui/ManagerCard/ManagerCard';
+import { fetchManager } from '@/lib/myData';
+import { ManagerField } from '@/lib/definitions';
 
-export default function Managers(){
-    return(
-        <div className="my-3">
-        <div className="text-3xl font-bold text-center text-red-700 py-3 ">Наши менеджеры</div>
-        <div className="flex flex-wrap gap-5 justify-center">
-        {managers.map((manager) => (
+const ManagersPage: React.FC = () => {
+    const [managers, setManagers] = useState<ManagerField[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-          <div key={manager._id} className="flex flex-col items-center my-5">
-            <div className="avatar flex flex-col items-center">
-              <div className="rounded-full">
-                <Image width={120} height={120} src={manager.image} alt="Ivan" />
-              </div>
-                <p className="font-bold text-2xl py-3">{manager.name}</p>
-                <p className="font-semibold text-md py-1">{manager.phone}</p>
-            </div>
-            <div className="flex gap-2 w-max justify-between mt-4">
- <a href={manager.viber} className="transition-transform transform hover:scale-110 "><Viber width={30} height={30} /></a>
- <a href={manager.telegram} className="transition-transform transform hover:scale-110 "><Telegram  width={30} height={30}/></a>
- <a href={manager.whatsapp} className="transition-transform transform hover:scale-110 "><WhatsApp width={30} height={30} /></a>
-                </div>
-          </div>
+    useEffect(() => {
+        const fetchManagers = async () => {
+            try {
+                const data = await fetchManager();
+                setManagers(data);
+            } catch (error) {
+                console.error("Failed to fetch managers:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
 
-        ))}
-      </div>
+        fetchManagers();
+    }, []);
+
+    if (isLoading) {
+        return <p>Загрузка...</p>;
+    }
+
+    return (
+        <div>
+            <ManagerCard managers={managers} />
         </div>
-       
-    )
-}
+    );
+};
+
+export default ManagersPage;
