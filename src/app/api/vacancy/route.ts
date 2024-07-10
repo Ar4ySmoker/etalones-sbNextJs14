507a17310e7aae3afd831c0e899ca496e0162a21
Your Name <your_email@example.com>
2024-07-11 00:21:37 +0300

    123

--- src/app/api/vacancy/route.ts
+++ src/app/api/vacancy/route.ts
@@ -6,7 +6,9 @@ export const GET = async () => {
     try {
         await connectToDB();
         console.log('mongo is conected')
-        const vacancy = await VacancyOnServer.find().populate('manager');
+        const vacancy = await VacancyOnServer.find().populate('manager')
+        .sort({ createdAt: -1 }) // Сортировка по убыванию даты создания
+        .limit(20);
         return new NextResponse(JSON.stringify(vacancy), { status: 200 });
     } catch (error) {
         return new NextResponse("error in fetching " + error, { status: 500 });
